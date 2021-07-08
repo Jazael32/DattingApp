a9c255c2e99acde31474e562b12d37b7aae35064
Jazael <martinjaza@gmail.com>
2021-07-08 00:26:19 -0500

    Add Paging, Sorting and Filtering (Api and Client)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                knownAs = user.KnownAs
+                knownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -70,7 +71,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                knownAs = user.KnownAs
+                knownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
